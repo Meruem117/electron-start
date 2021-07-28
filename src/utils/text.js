690d4d884615399f3d295ca6de6a5509b9f274b4
Meruem117 <1572677319@qq.com>
2021-07-29 00:01:13 +0800

    update text

--- src/utils/text.js
+++ src/utils/text.js
@@ -7,6 +7,7 @@ const {
     MenuItem,
     dialog
 } = remote
+const fs = require('fs')
 
 let currentFile = null // 当前文档保存的路径
 let isSaved = true // 当前文档是否已保存
@@ -46,6 +47,107 @@ const contextMenuTemplate = [{
 
 const contextMenu = Menu.buildFromTemplate(contextMenuTemplate)
 textEditor.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-    contextMenu.popup(remote.getCurrentWindow());
+    e.preventDefault()
+    contextMenu.popup(remote.getCurrentWindow())
 })
+
+// 监控文本框内容是否改变
+textEditor.oninput = e => {
+    if (isSaved) {
+        document.title += " *"
+    }
+    isSaved = false
+}
+
+//监听与主进程的通信
+ipcRenderer.on('action', (event, arg) => {
+    switch (arg) {
+        case 'new': //新建文件
+            askSaveIfNeed()
+            currentFile = null
+            txtEditor.value = ''
+            document.title = "Notepad - Untitled"
+            isSaved = true
+            break
+        case 'open': //打开文件
+            askSaveIfNeed()
+            const files = remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+                filters: [{
+                        name: "Text Files",
+                        extensions: ['txt', 'js', 'html', 'md']
+                    },
+                    {
+                        name: 'All Files',
+                        extensions: ['*']
+                    }
+                ],
+                properties: ['openFile']
+            })
+            if (files) {
+                currentFile = files[0]
+                const txtRead = readText(currentFile)
+                txtEditor.value = txtRead
+                document.title = "Notepad - " + currentFile
+                isSaved = true
+            }
+            break
+        case 'save': //保存文件
+            saveCurrentDoc()
+            break
+        case 'exiting':
+            askSaveIfNeed()
+            ipcRenderer.sendSync('reqaction', 'exit')
+            break
+    }
+})
+
+// 读取文本文件
+function readText(file) {
+    return fs.readFileSync(file, 'utf8')
+}
+
+// 保存文本内容到文件
+function saveText(text, file) {
+    fs.writeFileSync(file, text)
+}
+
+// 保存当前文档
+function saveCurrentDoc() {
+    if (!currentFile) {
+        const file = remote.dialog.showSaveDialog(remote.getCurrentWindow(), {
+            filters: [{
+                    name: "Text Files",
+                    extensions: ['txt', 'js', 'html', 'md']
+                },
+                {
+                    name: 'All Files',
+                    extensions: ['*']
+                }
+            ]
+        })
+        if (file) {
+            currentFile = file
+        }
+    }
+    if (currentFile) {
+        const txtSave = txtEditor.value
+        saveText(txtSave, currentFile)
+        isSaved = true
+        document.title = "Notepad - " + currentFile
+    }
+}
+
+// 如果需要保存，弹出保存对话框询问用户是否保存当前文档
+function askSaveIfNeed() {
+    if (isSaved) {
+        return
+    }
+    const response = dialog.showMessageBox(remote.getCurrentWindow(), {
+        message: 'Do you want to save the current document?',
+        type: 'question',
+        buttons: ['Yes', 'No']
+    })
+    if (response == 0) {
+        saveCurrentDoc() // 点击Yes按钮后保存当前文档}
+    }
+}
